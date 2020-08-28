a8b2f89e35ffbc85236879b8cccbe658679145eb
bxiaoyu1997 <853238349@qq.com>
2020-08-28 18:28:46 +0800

    dd

---
+++ requireDemo/js/a.js
@@ -0,0 +1,7 @@
+define(function(){
+   return {
+      say:function(name){
+         console.log('tag', name)
+      }
+   }
+});
---
+++ requireDemo/js/main.js
@@ -0,0 +1,12 @@
+require.config({
+    paths: {
+        "jquery": ["http://libs.baidu.com/jquery/2.0.3/jquery", "js/jquery"],
+        "a": "js/a"
+    }
+})
+
+require(["jquery","a"],function($,shabi){
+    
+})
+
+
